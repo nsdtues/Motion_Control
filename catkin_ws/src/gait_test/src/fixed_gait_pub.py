aa88541996b1464e6cd9d14a43ad19d54f1b492a
shiki_ubuntu <285056485@qq.com>
2018-05-28 13:27:23 +0800

    write log file in gait_test package

--- catkin_ws/src/gait_test/src/fixed_gait_pub.py
+++ catkin_ws/src/gait_test/src/fixed_gait_pub.py
@@ -12,11 +12,8 @@ class FIXED_GAIT_PUB(object):
 		rospy.init_node('fixed_gait_pub')
 		
 		self.cur_path = os.path.abspath(os.path.dirname(os.path.abspath(sys.argv[0])))	
-		self.pub_log_file_path = self.cur_path + "/../log/" + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + ".txt"
+		self.pub_log_file_path = self.cur_path + "/../log/" + "pubdata " + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + ".txt"
 		
-		with open(self.pub_log_file_path, "w") as self.GaitPubFile:
-		    print(self.pub_log_file_path)
-		    self.GaitPubFile.writelines(self.pub_log_file_path + '\n')
 		
 		# bind to gait stream
 		self.fixed_gait_stream = "tcp://*:8011"
@@ -24,14 +21,28 @@ class FIXED_GAIT_PUB(object):
 		self.fixed_gait_socket = self.fixed_gait_context.socket(zmq.PUB)
 		self.fixed_gait_socket.bind(self.fixed_gait_stream)
 
-		rate = rospy.Rate(100) # 100Hz
-		while not rospy.is_shutdown():
-			
-			self.gait = "GaitL:A, GaitR:B, Gait:GaitWalking"
-			self.fixed_gait_socket.send_string(self.gait)		
-			self.now = datetime.datetime.now()
-			print("pub {}, ".format(self.now) + self.gait)	
-			rate.sleep()
+		rate = rospy.Rate(67) # 67Hz
+		with open(self.pub_log_file_path, "w") as self.GaitPubFile:
+		    print(self.pub_log_file_path)
+		    # self.GaitPubFile.writelines(self.pub_log_file_path + '\n')
+		    self.gait_cnt = 0
+		    while not rospy.is_shutdown():
+			    self.gait_cnt = self.gait_cnt + 1
+			    if self.gait_cnt < 27:
+			        self.gait = "GaitL:A, GaitR:B, Gait:GaitWalking"
+			    elif self.gait_cnt < 60:
+			    	self.gait = "GaitL:B, GaitR:B, Gait:GaitWalking"
+			    elif self.gait_cnt < 100:
+			    	self.gait = "GaitL:C, GaitR:B, Gait:GaitWalking"
+			    else :
+			    	self.gait_cnt = 0
+			    
+			    self.fixed_gait_socket.send_string(self.gait)		
+			    self.now = datetime.datetime.now()
+			    nwrite = "{} ".format(self.now) + self.gait + "\n"
+			    self.GaitPubFile.writelines(nwrite)
+			    print("pub {}, ".format(self.now) + self.gait)	
+			    rate.sleep()
 
 
 if __name__ == '__main__':
--- catkin_ws/src/gait_test/src/gait_module_sub.py
+++ catkin_ws/src/gait_test/src/gait_module_sub.py
@@ -2,6 +2,7 @@
 
 import rospy
 import os
+import sys
 import datetime
 import zmq
 
@@ -15,6 +16,8 @@ class GAIT_MODULE_SUB(object):
 		
     def loop(self):	
 		
+		self.cur_path = os.path.abspath(os.path.dirname(os.path.abspath(sys.argv[0])))	
+		self.sub_log_file_path = self.cur_path + "/../log/" + "subdata " + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + ".txt"
 
 		self.gait_module_stream = "tcp://localhost:8011"
 		self.gait_module_context = zmq.Context()
@@ -27,10 +30,14 @@ class GAIT_MODULE_SUB(object):
 
 		self.gait_module_socket.setsockopt_string(zmq.SUBSCRIBE, self.dataFilter)		
 		
-		while not rospy.is_shutdown():
-			self.gaitstr = self.gait_module_socket.recv_string()
-			self.now = datetime.datetime.now()
-			print("sub {}, ".format(self.now) + self.gaitstr)	
+		with open(self.sub_log_file_path, "w") as self.GaitSubFile:
+			print(self.sub_log_file_path)
+			while not rospy.is_shutdown():
+				self.gaitstr = self.gait_module_socket.recv_string()
+				self.now = datetime.datetime.now()
+				nwrite = "{} ".format(self.now) + self.gaitstr + "\n"
+				self.GaitSubFile.writelines(nwrite)
+				print("sub {}, ".format(self.now) + self.gaitstr)	
 
 
 if __name__ == '__main__':
